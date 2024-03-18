e0234dfec34a29c91dd105a19c6128d0511ee1cf
Sofiia Kosachevych <sofiakosachvych@gmail.com>
2024-03-18 10:23:08 -0500

    useEffect increase coount

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from "react"
 
 function App() {
+
   const [count, setCount] = useState(0)
+
+  useEffect(() => {
+    document.title = `${count} new mwssages!`
+    console.log('run useEffect ', count);
+
+    return () => {
+      console.log('clean up ', count);
+    }
+  }, [count])
+
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h3>{count} new Messages!</h3>
+        <button onClick={() => setCount(count+1)}>Increate</button>
+        <button onClick={() => setCount(count-1)}>Decrease</button>
+        <button onClick={() => setCount(0)}>Reset</button>
+
+
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
