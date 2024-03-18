bcab1a2aee320b536400add33169a26b79663b59
Sofiia Kosachevych <sofiakosachvych@gmail.com>
2024-03-18 10:36:33 -0500

    timer in seconds

--- src/App.jsx
+++ src/App.jsx
@@ -2,26 +2,24 @@ import { useState, useEffect } from "react"
 
 function App() {
 
-  const [count, setCount] = useState(0)
+  const [time, setTime] = useState(0)
 
   useEffect(() => {
-    document.title = `${count} new mwssages!`
-    console.log('run useEffect ', count);
+    const timer = setInterval(() => {
+      setTime(time + 1)
+    }, 1000)
+
 
     return () => {
-      console.log('clean up ', count);
+      clearInterval(timer) // This ensures that the interval is stopped when the component is unmounted or when the time variable changes.
     }
-  }, [count])
+  }, [time])
 
 
   return (
     <>
       <div>
-        <h3>{count} new Messages!</h3>
-        <button onClick={() => setCount(count+1)}>Increate</button>
-        <button onClick={() => setCount(count-1)}>Decrease</button>
-        <button onClick={() => setCount(0)}>Reset</button>
-
+        <h3>{time} in seconds!</h3>
 
       </div>
     </>
